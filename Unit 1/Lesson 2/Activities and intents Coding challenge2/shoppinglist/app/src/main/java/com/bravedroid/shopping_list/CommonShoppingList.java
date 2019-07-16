0c336772cbd1be29234af243ea5c953279044fba
refka-farid <refka.farid@gmail.com>
2019-07-16 11:42:18 +0200

    [learning] finalize shopping_list app.

--- Unit 1/Lesson 2/Activities and intents Coding challenge2/shoppinglist/app/src/main/java/com/bravedroid/shopping_list/CommonShoppingList.java
+++ Unit 1/Lesson 2/Activities and intents Coding challenge2/shoppinglist/app/src/main/java/com/bravedroid/shopping_list/CommonShoppingList.java
@@ -3,9 +3,19 @@ package com.bravedroid.shopping_list;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class CommonShoppingList extends AppCompatActivity {
+    private static final String LOG_TAG = CommonShoppingList.class.getSimpleName();
+    private static final int[] BUTTONS = {
+            R.id.beans, R.id.milk, R.id.bread, R.id.cheese,
+            R.id.fish, R.id.cucumber, R.id.chocolate,
+            R.id.garlic, R.id.meat, R.id.nuts, R.id.pea,
+            R.id.salad, R.id.toilet_paper, R.id.vegetables, R.id.chicken
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +23,18 @@ public class CommonShoppingList extends AppCompatActivity {
         setContentView(R.layout.activity_common_shopping_list);
     }
 
-    public void buildList(View view) {
+    public void onItemButtonClick(View view) {
         Intent myListShoppingIntent = new Intent(this, MainActivity.class);
-        startActivity(myListShoppingIntent);
+        for (int button1 : BUTTONS) {
+            Button button = findViewById(button1);
+            if (view == button) {
+                String stringToPassBack = button.getText().toString();
+                Log.d(LOG_TAG, "****************" + stringToPassBack + "****************");
+                myListShoppingIntent.putExtra("result", stringToPassBack);
+                setResult(RESULT_OK, myListShoppingIntent);
+                finish();
+                break;
+            }
+        }
     }
 }
--- Unit 1/Lesson 2/Activities and intents Coding challenge2/shoppinglist/app/src/main/java/com/bravedroid/shopping_list/MainActivity.java
+++ Unit 1/Lesson 2/Activities and intents Coding challenge2/shoppinglist/app/src/main/java/com/bravedroid/shopping_list/MainActivity.java
@@ -1,20 +1,101 @@
 package com.bravedroid.shopping_list;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String RESULT = "result";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int COMMON_SHOPPING_LIST_ACTIVITY_REQUEST_CODE = 1;
+    private static final String MY_SHOPPING_LIST_KEY = "savedElementsList";
+    private ArrayList<String> savedElementsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = findViewById(R.id.myListShopping);
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        if (savedInstanceState != null) {
+            ArrayList<String> myShoppingList = savedInstanceState.getStringArrayList(MY_SHOPPING_LIST_KEY);
+            if (myShoppingList != null) {
+                for (String item : myShoppingList) {
+                    Log.d(LOG_TAG, "*************" + item + "************");
+                    if (savedElementsList.size() < 10) {
+                        if (!savedElementsList.contains(item)) {
+                            TextView tv = new TextView(this);
+                            tv.setLayoutParams(lparams);
+                            createTextView(linearLayout, tv, item);
+                            savedElementsList.add(item);
+                        }
+                    } else {
+                        Toast toast = Toast.makeText(this, "YOU HAVE TO SAVE ONLY 10 ITEMS", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }
+        }
     }
 
-    public void addItem(View view) {
+    public void onAddItemButtonClick(View view) {
         Intent allListShoppingIntent = new Intent(this, CommonShoppingList.class);
-        startActivity(allListShoppingIntent);
+        startActivityForResult(allListShoppingIntent, COMMON_SHOPPING_LIST_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        LinearLayout linearLayout = findViewById(R.id.myListShopping);
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    String result = data.getStringExtra(RESULT);
+                    if (savedElementsList.size() < 10) {
+                        if (!savedElementsList.contains(result)) {
+                            TextView tv = new TextView(this);
+                            tv.setLayoutParams(lparams);
+                            createTextView(linearLayout, tv, result);
+                            savedElementsList.add(result);
+                        }
+                    } else {
+                        Toast toast = Toast.makeText(this, "YOU HAVE TO SAVE ONLY 10 ITEMS", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void createTextView(LinearLayout linearLayout, TextView tv, String text) {
+        tv.setTextSize(20.0f);
+        tv.setTextColor(Color.RED);
+        tv.setAllCaps(true);
+        tv.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+        tv.setText(text);
+        linearLayout.addView(tv);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (savedElementsList.size() > 0) {
+            outState.putStringArrayList(MY_SHOPPING_LIST_KEY, savedElementsList);
+        }
     }
 }
