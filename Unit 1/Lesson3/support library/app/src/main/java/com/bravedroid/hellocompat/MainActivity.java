d7a2416211ed5d150d1e2898376639aa91da69d8
refka-farid <refka.farid@gmail.com>
2019-08-05 10:50:22 +0200

    [learning] finalize helloCompat project

--- Unit 1/Lesson3/support library/app/src/main/java/com/bravedroid/hellocompat/MainActivity.java
+++ Unit 1/Lesson3/support library/app/src/main/java/com/bravedroid/hellocompat/MainActivity.java
@@ -1,8 +1,9 @@
 package com.bravedroid.hellocompat;
 
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
             "indigo", "blue", "light_blue", "cyan", "teal", "green",
             "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
-            "brown", "grey", "blue_grey", "black" };
+            "brown", "grey", "blue_grey", "black"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +26,24 @@ public class MainActivity extends AppCompatActivity {
             mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
         }
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         // save the current text color
         outState.putInt("color", mHelloTextView.getCurrentTextColor());
     }
+
     public void changeColor(View view) {
         Random random = new Random();
         String colorName = mColorArray[random.nextInt(20)];
-        int colorResourceName = getResources().getIdentifier(colorName,
-                "color", getApplicationContext().getPackageName());
-        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+        int colorRes;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            colorRes = getResources().getColor(colorResourceName, this.getTheme());
+        } else {
+            colorRes = ContextCompat.getColor(this, colorResourceName);
+        }
         mHelloTextView.setTextColor(colorRes);
     }
 }
